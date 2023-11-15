ab63cc6680519d9285b00f3f41426dfe2ebea0cc
rohitXkumar <111526128+rohitXkumar@users.noreply.github.com>
2023-11-15 12:31:40 +0530

    Add files via upload

---
+++ linkedin-clone-main/src/components/post.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function post(){
+    return(
+        <div class name ="Posts">
+            <div className="post_Header">
+                <div className="post_Header Left">
+                <Avatra/>
+                <div className="post_profile_details">
+                    <h3>Rohit Kumar</h3>
+                    <p>I am Learning React js</p>
+                </div>
+                </div>
+                <div className="post_Header Right">
+
+                </div>
+            </div> 
+
+        </div>
+    )
+}
+export default post
